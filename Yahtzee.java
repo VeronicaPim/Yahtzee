90656b6c2941af5bab7652e0bab187fc795cbc32
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2023-03-08 17:01:16 -0500

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -3,7 +3,6 @@
   This program will represent the single player Yahtzee game, based on the rules given.
   It will use the Player class, which in turn uses the Dice and Scorecard classes.*/
 
-
   //imports
   import java.util.Scanner;
 
