82d948e9bb78220fafc90e8c0a4f5f4e90d71fad
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2023-04-05 18:00:21 -0400

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,10 +1,7 @@
 /*Veronica Pimenova
   Period 6
   This will represent the Dice class, with rolling and rerolling dice.*/
-public class Dice{
-	
-	
-	
+public class Dice{	
      //instance variables
      private int[] diceValues;
 
