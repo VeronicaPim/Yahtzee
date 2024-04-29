a320022a41c00f71d4f2ad7170a979acaff33c51
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2024-04-29 15:57:06 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,8 @@
 /*Veronica Pimenova
   Period 6
   This will represent the Player class for the Yahtzee project. It will
-  deal with the player's affairs.*/
+  deal with the player's
+  affairs.*/
 
 //imports
 import java.util.Scanner;
