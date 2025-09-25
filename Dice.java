b4fa46f7f0066afcbcefe16b9819811109fd979e
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2025-09-25 13:38:55 -0400

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -2,6 +2,7 @@
   Period 6
   This will represent the Dice class, with rolling and rerolling dice.*/
 public class Dice{
+
 	
      //instance variable
      private int[] diceValues;
@@ -38,6 +39,7 @@ public class Dice{
 		 diceValues[die] = (int)(Math.random()*6+1);
 	 }
 }
+
 
 
 
